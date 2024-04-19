fa94546736190fc3a799ef741f31dbdf18d50027
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2024-04-19 14:43:05 +0100

    ajou

---
+++ js/ajouter.js
@@ -0,0 +1,26 @@
+const date = document.querySelector("#exampleInputDate");
+const ville = document.querySelector("#exampleInputVille");
+const nom = document.querySelector("#exampleInputNom");
+const ajouter = document.querySelector("button");
+const street = document.querySelector("#exampleInputStreet");
+
+let ajouterMagazins = JSON.parse(localStorage.getItem("magazins")) || [];
+
+//AJOUTER
+function addMagazins() {
+    ajouterMagazins.push({
+        name : nom.value,
+        city : ville.value,
+        street : street.value,
+        date : date.value
+    });
+
+    localStorage.setItem("nagazins", JSON.stringify(ajouterMagazins));
+}
+
+ajouter.addEventListener("click", (e)=> {
+    e.preventDefault();
+    addMagazins();
+    window.location.reload();
+    console.log(date.value, ville.value, nom.value);
+})
