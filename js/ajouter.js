2c14d086efde2caf972db9a3acd3b5f64b0f3ca9
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2024-04-20 19:50:21 +0100

    Update ajouter

--- js/ajouter.js
+++ js/ajouter.js
@@ -8,21 +8,26 @@ let ajouterMagazins = JSON.parse(localStorage.getItem("magazins")) || [];
 
 //AJOUTER
 function addMagazins() {
-    if (!nom.value || !ville.value || !street.value || !date.value)
-    {
-        alert("enter all inputs");
+    if (!nom.value || !ville.value || !street.value || !date.value) {
+        alert("Please enter all inputs");
+    } else {
+        // Check if the magazine already exists
+        const nomMagazine = ajouterMagazins.find(magazine => magazine.name === nom.value);
+        if (nomMagazine) {
+            alert(nomMagazine.name + " already exists");
+        } else {
+            // Add new magazine entry
+            ajouterMagazins.push({
+                name: nom.value,
+                city: ville.value,
+                street: street.value,
+                year: date.value
+            });
+            // Save to local storage
+            localStorage.setItem("magazins", JSON.stringify(ajouterMagazins));
+            alert("Magazine added successfully");
+        }
     }
-    else
-    {
-        ajouterMagazins.push({
-            name : nom.value,
-            city : ville.value,
-            street : street.value,
-            date : date.value
-        });
-    }  
-
-    localStorage.setItem("magazins", JSON.stringify(ajouterMagazins));
 }
 
 ajouter.addEventListener("click", (e)=> {
